bae2b3973aed030d81db5491afe55e395a52d4e4
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2026-05-30 09:27:42 -0300

    feat: add DashboardStats domain model

---
+++ apps/server/src/domain/models/dashboard.ts
@@ -0,0 +1,45 @@
+export type LatestRegistryEntry = {
+	id: number;
+	title: string;
+	clockIn: Date;
+	clockOut: Date;
+	observations: string | null;
+	createdAt: Date;
+	createdBy: string;
+	userName: string;
+	userEmail: string;
+};
+
+export type MonthlyCount = {
+	month: string;
+	count: number;
+};
+
+export type MonthlyHours = {
+	month: string;
+	hours: number;
+};
+
+export type OvertimeSummary = {
+	totalOvertimeHours: number;
+	weekdayAfter17Hours: number;
+	saturdayHours: number;
+};
+
+export type WeeklyPresenceEntry = {
+	dayOfWeek: number;
+	dayName: string;
+	users: number;
+};
+
+export type DashboardStats = {
+	activeWorkers: number;
+	clockedInToday: number;
+	notClockedInToday: number;
+	lateClockInsPerMonth: MonthlyCount[];
+	overtimeSummary: OvertimeSummary;
+	avgHoursPerDay: number;
+	weeklyPresence: WeeklyPresenceEntry[];
+	extraHoursLast5Months: MonthlyHours[];
+	latestRegistries: LatestRegistryEntry[];
+};
